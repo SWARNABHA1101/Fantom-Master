6be520d9bc796ebff59a8d472b340ce6840f2882
alex0xhodler <97283568+alex0xhodler@users.noreply.github.com>
2022-10-12 23:47:07 +0200

    Update FSComunity.jsx

--- src/components/FsCommunity/FSComunity.jsx
+++ src/components/FsCommunity/FSComunity.jsx
@@ -11,24 +11,6 @@ const FSComunity = () => {
         <p>
           FS token holders get to be part of the FSmart Club while the Liquid subscription is enabling for them earning opportunities with fixed and variable APY.
         </p>
-        <div className="Earn Aptos">
-          <div className="Comunitycard">
-            <div className="img">{/* <img src={image} alt="" /> */}</div>
-            <div className="ComunitycardBottom">
-              <h1>How-to</h1>
-              <p>FS Project Post IDO Review:</p>
-              <span>$MGCT &gt; Dec 2021</span>
-              <div class="fBton">
-                <div class="fbtn1">
-                  <button>How to invest</button>
-                  <FontAwesomeIcon
-                    icon={faCircleChevronRight}
-                    className="arrowicons"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
           <div className="Earn $CHRP">
             <div className="img">{/* <img src={image} alt="" /> */}</div>
             <div className="ComunitycardBottom">
