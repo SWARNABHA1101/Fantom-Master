2a16230021ae8fcb90ab9ae78dc860f1581cb16a
alex0xhodler <97283568+alex0xhodler@users.noreply.github.com>
2022-10-12 22:32:28 +0200

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,12 +8,9 @@ const Header = () => {
     <div class="header">
     <div class="upper">
         <div class="head-left">
-            <h4>opportunity Awaits</h4>
-            <h1>Invest or raise capital
-                on any blockchain</h1>
-                <p>We’re an opportunity starter that enables investors to participate first in the most promising
-                    projects and products with solid business plans to win top blockchain opportunities. Hold $FS
-                    to access hand-picked projects’ token sales and other distinctive features.</p>
+            <h4>Audited. Automated. AI powered</h4>
+            <h1>Your investment wallet</h1>
+                <p>Start investing in web3 native crypto startups with FS wallet. With an easy to use mobile wallet app, we area the most secure investment wallet on the market. Get access to the hottest token and NFT deals across L1s and L2s in a decentralized way with FS tokens and FS Key NFTs</p>
                 </div>
             <div class="head-right">
                     <img src="images/head.png" alt=""/>
@@ -21,25 +18,25 @@ const Header = () => {
     </div>
     <div class="lower">
         <div class="btn">
+            <button>Get the app</button>
+            <FontAwesomeIcon  icon={faCircleChevronRight} className="arrowicon" />
+        </div>
+        <div class="btn">
             <button>How to invest</button>
             <FontAwesomeIcon  icon={faCircleChevronRight} className="arrowicon" />
         </div>
         <div class="btn">
-            <button>Tier benefits</button>
+            <button>FSmart Club</button>
             <FontAwesomeIcon  icon={faCircleChevronRight} className="arrowicon" />
         </div>
         <div class="btn">
-            <button>app.fantomstarter</button>
-            <FontAwesomeIcon  icon={faCircleChevronRight} className="arrowicon" />
-        </div>
-        <div class="btn">
-            <button>Apply as a Project</button>
+            <button>Apply to Web3 Accelerator</button>
             <FontAwesomeIcon  icon={faCircleChevronRight} className="arrowicon" />
         </div>
        
     </div>
     <div className="bottom">
-        <img src="images/fontom.png" alt="" />
+        <img src="images/fantom.png" alt="" />
         <img src="images/fontom.png" alt="" />
         <img src="images/fontom.png" alt="" />
         <img src="images/fontom.png" alt="" />
