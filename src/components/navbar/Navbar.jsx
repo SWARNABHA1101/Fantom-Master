117ff43f4c297ea6df9638a34e8776f43164a293
alex0xhodler <97283568+alex0xhodler@users.noreply.github.com>
2022-10-12 22:23:43 +0200

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
                 <ul>
                     <li><a href="#">Projects</a></li>
                     <li><a href="#">FSmart Club</a></li>
-                    <li><a href="#">Governance</a></li>
+                    <li><a href="#">Earn</a></li>
                     <li><a href="#">Blog</a></li>
                     <li><a href="#">Web3 Accelerator Program</a></li>
                 </ul>
